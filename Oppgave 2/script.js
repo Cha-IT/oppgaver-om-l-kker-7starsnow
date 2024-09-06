2c77b0346186c543f85904d32474f386f8ed00a0
7starsnow <runar@svenning.co>
2024-09-06 14:23:18 +0200

    Sterted the project

---
+++ Oppgave 2/script.js
@@ -0,0 +1,10 @@
+const password = document.getElementById('password')
+
+
+start.onclick = () => {
+let sign = prompt("What is the password to your acount");
+
+    while (sign === password) {
+        start;
+    }
+}
