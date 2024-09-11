9cd44a34122c71c2d9ec09d29251fed86c69c5a0
7starsnow <runar@svenning.co>
2024-09-11 16:26:00 +0200

    task 3 done

--- Oppgave 3/script.js
+++ Oppgave 3/script.js
@@ -4,9 +4,20 @@ const div = document.getElementById('answer')
 
 btnEven.onclick = () => {
     let number = 0;
+    div.innerHTML = ''
 
     while (number < 100) {
-        number =+2;
+        number += 2;
+        div.innerHTML += number + '<br>';
+    }
+}
+
+btnOdd.onclick = () => {
+    let number = 0;
+    div.innerHTML = ''
+
+    while (number < 100) {
+        number++;
         div.innerHTML += number + '<br>';
     }
 }
