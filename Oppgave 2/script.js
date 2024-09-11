d2954fd4b321ff7d55497d39cd47ef1e6d1d9936
7starsnow <runar@svenning.co>
2024-09-11 15:47:37 +0200

    Task 2 done

--- Oppgave 2/script.js
+++ Oppgave 2/script.js
@@ -1,10 +1,27 @@
 const password = document.getElementById('password')
+const start = document.getElementById('start')
+const answer = document.getElementById('answer')
 
 
 start.onclick = () => {
-let sign = prompt("What is the password to your acount");
+let sign;
+let attempts = 0;
 
-    while (sign === password) {
-        start;
+sign = prompt("What is the password to your acount");
+
+    while (sign !== password.value && attempts < 3) {
+        attempts++;
+
+        if (attempts === 3) {
+            alert('Acount locked. Too many attepts.');
+            return;
+        }
+
+        alert('Not correct')
+        sign = prompt("What is the password to your acount");
+        
     }
-}
+    
+        alert('Password correct')
+        answer.innerHTML = 'Welcome'
+};
